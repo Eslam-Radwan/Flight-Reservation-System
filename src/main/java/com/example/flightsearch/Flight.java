ef291c2d8ffa50b1dd7eb52383c4a9e51f02a2e9
unknown <osans026@gmail.com>
2023-12-14 14:15:44 +0200

    Added the Following Scenes Together : FlightSearch,FlightSelect,BookingScene,SeatSelection

---
+++ src/main/java/com/example/flightsearch/Flight.java
@@ -0,0 +1,156 @@
+package com.example.flightsearch;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Flight implements Serializable {
+    private int flightNumber;
+    private String departureAirport;
+    private String arrivalAirport;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
+    private LocalDate departureDate;
+    private double[] seatPrice;
+    private Seat[][] economySeats;
+    private Seat[][] businessClass;
+    private Seat[][] firstClass;
+    private int[] numberOfAvailableSeat ;
+//    ArrayList<String>  passenger = new ArrayList<>();
+
+    public void setEconomySeats(Seat[][] economySeats) {
+        this.economySeats = economySeats;
+    }
+
+    public void setBusinessClass(Seat[][] businessClass) {
+        this.businessClass = businessClass;
+    }
+
+    public void setFirstClass(Seat[][] firstClass) {
+        this.firstClass = firstClass;
+    }
+
+    public Flight(){
+        seatPrice = new double[3];
+        numberOfAvailableSeat = new int[]{30, 30, 30};
+
+        economySeats = new Seat[5][6];
+        for (int i=0;i<5;i++){
+            for (int j=0;j<6;j++){
+                economySeats[i][j]=new Seat(i+j,0,true,i,j);
+            }
+        }
+        businessClass = new Seat[5][6];
+        for (int i=0;i<5;i++){
+            for (int j=0;j<6;j++){
+                businessClass[i][j]=new Seat(i+j,0,true,i,j);
+            }
+        }
+        firstClass = new Seat[5][6];
+        for (int i=0;i<5;i++){
+            for (int j=0;j<6;j++){
+                firstClass[i][j]=new Seat(i+j,0,true,i,j);
+            }
+        }
+    }
+    public int getNumberOfAvailableSeat(int classType){
+        return this.numberOfAvailableSeat[classType];
+    }
+
+    @Override
+    public boolean equals(Object object){
+        Flight flight = (Flight)object;
+        if(this.departureAirport.equals(flight.departureAirport) && this.arrivalAirport.equals(flight.arrivalAirport) && this.departureDate.equals(flight.getDepartureDate())) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public void setArrivalTime(LocalTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public void setSeatPrice(int seatClass,double price) {
+        this.seatPrice[seatClass] = price;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public LocalTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public double getSeatPrice(int seatClass) {
+        return seatPrice[seatClass];
+    }
+
+    public Seat[][] getSeats(int seatClass) {
+        if(seatClass == 0){
+            return economySeats;
+        }
+        else if(seatClass == 1){
+            return businessClass;
+        }
+        else{
+            return firstClass;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "flightNumber=" + flightNumber +
+                ", departureAirport='" + departureAirport + '\'' +
+                ", arrivalAirport='" + arrivalAirport + '\'' +
+                ", departureTime=" + departureTime +
+                ", arrivalTime=" + arrivalTime +
+                ", departureDate=" + departureDate +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/flightsearch/Ticket.java
@@ -0,0 +1,62 @@
+package com.example.flightsearch;
+
+import java.io.Serializable;
+
+public class Ticket implements Serializable {
+    private static int previousTicketNumber;
+    private int ticketNumber;
+    private double ticketFare;
+    private Passenger passenger;
+    private Seat passengerSeat;
+
+    public Ticket(Passenger passenger, double ticketFare) {
+        ticketNumber = previousTicketNumber + 1;
+        previousTicketNumber++;
+        this.passenger = passenger;
+        this.ticketFare = ticketFare;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public double getTicketFare() {
+        return ticketFare;
+    }
+
+    public void setTicketFare(double ticketFare) {
+        this.ticketFare = ticketFare;
+    }
+
+    public Seat getPassengerSeat() {
+        return passengerSeat;
+    }
+
+    public void setPassengerSeat(Seat passengerSeat) {
+        this.passengerSeat = passengerSeat;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    @Override
+    public String toString() {
+        return " Ticket:" + "\n" +
+                " TicketNumber: " + ticketNumber + "\t" +
+                "TicketFare: " + ticketFare + "\n" +
+                " passenger: " + passenger.getFirstName() + " " + passenger.getLastName() + "\t" + "Passenger Id: " + passenger.getID() +
+                ", Seat number: " + passengerSeat.getSeatNumber() +
+                "\n Seat number: " + passengerSeat.getSeatNumber() + "\t" +
+                " Class: " + passengerSeat.getClass()
+                ;
+    }
+}
