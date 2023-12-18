37eb94d634bb307e6f52c16c81aa4adba6cb9892
Radwan <islamshoman1111@gmail.com>
2023-12-18 11:50:29 +0200

    Admin Page Final Edits4

--- src/main/java/edu/asu/flightreservationsystem/WorkFlow.java
+++ src/main/java/edu/asu/flightreservationsystem/WorkFlow.java
@@ -9,6 +9,24 @@ import java.util.ArrayList;
 public class WorkFlow implements Serializable{
     public static ArrayList<Flight> Flights = new ArrayList<>();
     public static ArrayList<ApplicationUser> appUsers = new ArrayList<>();
+    public static ArrayList<Airport> airports = new ArrayList<>();
+
+    public void preWork(Stage priamryStage){
+        loadData();
+        Admin localAdmin = makeLocalAdmin();
+        appUsers.add(localAdmin);
+        User localUser = makeLocalUser();
+        appUsers.add(localUser);
+
+        /*
+        boolean doWork;
+        do {
+            doWork = work();
+        }while(doWork);
+        uploadData();
+
+         */
+    }
 
     public void work(Stage priamryStage)
     {
